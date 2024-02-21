9012e3c67e4f909e514bb0606ba0ea417a1862ad
Ivan Kiiashko <ivan.m.kiiashko@gmail.com>
2024-02-21 12:35:09 -0500

    3

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import user from 'user.json';
 import data from 'data.json';
+import friends from 'friends.json';
 import Profile from './Profile';
 import Statistics from './Statistics';
+import FriendList from './FriendList';
 
 function App() {
   return (
     <>
       <Profile {...user} />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 }
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,14 @@
+import css from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => {
+        return <FriendListItem key={friend.id} {...friend} />;
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
